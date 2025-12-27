de136bb7b4b88e11bccee6fe997e0a102fc18a17
Dhruv Bhavsar <40565844+dbhavsar76@users.noreply.github.com>
2025-12-27 11:39:34 +0530

    update SettingsAboutScreen with preference elements

--- app/src/main/java/com/nutomic/syncthingandroid/settings/SettingsAboutScreen.kt
+++ app/src/main/java/com/nutomic/syncthingandroid/settings/SettingsAboutScreen.kt
@@ -1,10 +1,15 @@
 package com.nutomic.syncthingandroid.settings
 
+import android.content.Intent
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.stringResource
 import androidx.navigation3.runtime.EntryProviderScope
 import com.nutomic.syncthingandroid.R
+import com.nutomic.syncthingandroid.activities.LicenseActivity
+import me.zhanghai.compose.preference.Preference
 
 
 fun EntryProviderScope<SettingsRoute>.settingsAboutEntry() {
@@ -16,9 +21,50 @@ fun EntryProviderScope<SettingsRoute>.settingsAboutEntry() {
 
 @Composable
 fun SettingsAboutScreen() {
+    val context = LocalContext.current
+    val uriHandler = LocalUriHandler.current
+
     SettingsScaffold(
         title = stringResource(R.string.category_about),
     ) {
-        Text(stringResource(R.string.category_about))
+        Preference(
+            title = { Text(stringResource(R.string.app_version_title)) },
+            summary = { Text("TODO: app version") },
+        )
+        Preference(
+            title = { Text(stringResource(R.string.syncthing_version_title)) },
+            summary = { Text("TODO: st version") },
+        )
+        Preference(
+            title = { Text(stringResource(R.string.syncthing_database_size)) },
+            summary = { Text("TODO: db size") },
+        )
+        Preference(
+            title = { Text(stringResource(R.string.os_open_file_limit)) },
+            summary = { Text("TODO: open file limit") },
+        )
+
+        val stForumUri = stringResource(R.string.syncthing_forum_url)
+        Preference(
+            title = { Text(stringResource(R.string.syncthing_forum_title)) },
+            summary = { Text(stringResource(R.string.syncthing_forum_summary)) },
+            onClick = { uriHandler.openUri(stForumUri) },
+        )
+
+        val stPrivacyPolicyUri = stringResource(R.string.privacy_policy_url)
+        Preference(
+            title = { Text(stringResource(R.string.privacy_title)) },
+            summary = { Text(stringResource(R.string.privacy_summary)) },
+            onClick = { uriHandler.openUri(stPrivacyPolicyUri) },
+        )
+
+        Preference(
+            title = { Text(stringResource(R.string.open_source_licenses_title)) },
+            summary = { Text(stringResource(R.string.open_source_licenses_summary)) },
+            onClick = {
+                val intent = Intent(context, LicenseActivity::class.java)
+                context.startActivity(intent)
+            },
+        )
     }
 }
